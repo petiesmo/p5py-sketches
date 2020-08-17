3c99b0b3670da4e8402ab6d8efd9f8e28f634ac1
Peter S <petiesmo@gmail.com>
2020-08-17 13:28:41 -0600

    Most code for radial spray

--- Fireworks.py
+++ Fireworks.py
@@ -4,12 +4,26 @@ G = (0,0.25)
 particles = None
 hz = None
 
-class Particle(Vector):
-    def __init__(self,p,v,a):
+def polar2rect(r,th,deg=False):
+	'''Conv 2d polar coords to x,y'''
+	th = th*PI/180 if deg else th
+	return (r*cos(th), r*sin(th))
+
+class myVector(Vector):
+	def __init__(self,x,y,z=0):
+		super().__init__(0,1)
+		
+	def.set_mag_dir(m,d,deg=False):
+		self.x,self.y = polar2rect(m,d,deg)
+
+class Particle():
+    def __init__(self,p,v,a,e=False):
         self.pos = Vector(*p)
         self.vel = Vector(*v)
         self.acc = Vector(*a)
         self.color = (255*random_uniform(), 255*random_uniform(), 255*random_uniform())
+		self.exploded = e
+		return None
 
     def update(self):
         self.vel += self.acc
@@ -22,6 +36,17 @@ class Particle(Vector):
         circle(self.pos,size)
         return None
 
+	def explode(self,n):
+		#Make list of new particles, with state = parent
+		self.exploded = True
+		self.children = []
+		for i in range n:
+			vfire = myVector(1,0).set_mag_dir(1, 5*i, True)
+			adrag = -Cd*vfire**
+			# New Particles also get radial velocity, and some drag(neg radial acc)
+			self.children.append(Particle(self.pos, self.vel+vfire, self.acc+adrag,True))
+		
+
 def setup():
     global G,particles,hz
     size(1000,1500)
@@ -32,6 +57,7 @@ def setup():
 
 def draw():
     global G, particles,hz
+	# New particles appear randomly
     if random_uniform() < 0.1:
         particles.append(Particle((width*random_uniform(),hz),(0,-20),G))
 
@@ -40,17 +66,20 @@ def draw():
     line((0,hz),(width,hz))
     
     for p in particles:
-        p.update()
-        
+        # New pos, vel, acc
+		p.update()
+        # Die off
         if p.pos.y > 3*height/4:
             particles.remove(p)
-        if p.vel.y > 0:
-            p.show(size=20)
+        # Boom!
+		if p.vel.y > 0 and not p.exploded
+            p.explode(20)
+			p.show(size=20)	#TODO Replace with burst of new particles
         else:
             p.show()
 
     return None
-    
 
-run()
     
+if __name__=='__main__':
+	run()
