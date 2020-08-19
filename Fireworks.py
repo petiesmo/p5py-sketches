0aa61ef48a86b1735fb54392ef88e086c18a2c78
Peter S <petiesmo@gmail.com>
2020-08-18 23:20:21 -0600

    Improved random calls, Changed children and drag

--- Fireworks.py
+++ Fireworks.py
@@ -1,8 +1,10 @@
 from p5 import *
+from random import randint as ri
+from random import randrange as rr
 
-G = (0,0.25)
+G = Vector(0,0.25)
 particles = None
-hz = None
+hzn = None
 
 def polar2rect(r,th,deg=False):
 	'''Conv 2d polar coords to x,y'''
@@ -15,18 +17,16 @@ class mdVector(Vector):
 		super().__init__(x,y)
 
 class Particle():
-    def __init__(self,p,v,a,e=False):
+    def __init__(self,p,v,a,ex=False):
         self.pos = Vector(*p)
         self.vel = Vector(*v)
         self.acc = Vector(*a)
-        self.color = (255*random_uniform(), 255*random_uniform(), 255*random_uniform())
-		self.exploded = e
-		self.children = []
+        self.color = (ri(0,255), ri(0,255), ri(0,255))
+		self.exploded = ex
 		return None
 
     def update(self):
-		for c in self.children:
-			 
+		self.acc = G + self.drag()
 		self.vel += self.acc
         self.pos += self.vel
         return None
@@ -35,55 +35,54 @@ class Particle():
         stroke(255)
         fill(*self.color)
         circle(self.pos,size)
-		[circle(i.pos,4) for i in self.children]
         return None
 
 	def explode(self,n):
 		#Make list of new particles, with state = parent
 		self.exploded = True
-		self.children = []
-		for i in range n:
+		children = []
+		for i in range(n):
 			# New Particles get radial velocity, and some drag (neg radial acc)
 			vfire = mdVector(10, 5*i, True)
-			self.children.append(Particle(self.pos, self.vel+vfire, self.acc+self.drag(vfire),True))
-		return None
+			children.append(Particle(self.pos, self.vel+vfire, G+self.drag(),True))
+		return children
 
-		def drag(self, v):
-			return mdVector(v.magnitude**2, v.direction+180,True)
+		def drag(self):
+			return mdVector(self.vel.magnitude**2, self.vel.direction+180,True)
+			
 #End Particle class
 		
 
 def setup():
-    global G,particles,hz
+    global G,particles,hzn
     size(1000,1500)
     title('Fireworks')
-    hz = height*2/3
-    particles = [Particle((width*random_uniform(),height),(0,-20),G) for i in range(7)]
+    hzn = height*2/3
+    particles = [Particle((rr(width),hzn),(rr(-3,3),rr(-23,17)),G) for i in range(7)]
     return None
 
 def draw():
-    global G, particles,hz
+    global G, particles,hzn
 	# New particles appear randomly
     if random_uniform() < 0.1:
-        particles.append(Particle((width*random_uniform(),hz),(0,-20),G))
+        particles.append(Particle((rr(width),hzn),(0,-20),G))
 
     background(0)
     stroke(255)
-    line((0,hz),(width,hz))
-    
+    line((0,hzn),(width,hzn))
+	temp = []
     for p in particles:
         # New pos, vel, acc
 		p.update()
-        # Die off
-        if p.pos.y > 3*height/4:
-            particles.remove(p)
         # Boom!
 		if p.vel.y > 0 and not p.exploded
-            p.explode(20)
-			p.show(size=20)	#TODO Replace with burst of new particles
-        else:
-            p.show()
-
+            temp.extend(p.explode(20))# Burst of new particles
+		# Die off
+        if p.pos.y > 3*height/4:
+            particles.remove(p)
+	# Display all points
+	particles.extend(temp)
+	(p.show() for p in particles)
     return None
 
     
